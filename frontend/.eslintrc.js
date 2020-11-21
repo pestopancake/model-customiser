5fcec9573b32672eb9d0374e6242636f39745872
Paul <p@ulmart.in>
2020-11-21 11:50:52 +0000

    eslint

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -11,8 +11,10 @@ module.exports = {
     parser: 'babel-eslint'
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'vue/no-unused-components': 'warn',
+    'vue/no-unused-vars': 'warn'
   },
   overrides: [
     {
