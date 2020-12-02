731d904d10af6992f2944f9343c0ed0e56c7da91
Paul <p@ulmart.in>
2020-12-02 21:47:27 +0000

    api

--- frontend/apisrc/lib/db.js
+++ frontend/apisrc/lib/db.js
@@ -2,11 +2,6 @@ const mongodb = require("./mongodb.js");
 
 module.exports = {
     db: null,
-    async allQuotes() {
-        return await this.run(async function(mongodb) { 
-            return await mongodb.collection('quotes').find({}).toArray();
-        })
-    },
     async run(callback) {
         if (!this.db) { 
             this.db = await (await mongodb.connect()).db();
--- frontend/apisrc/repositories/quote.js
+++ frontend/apisrc/repositories/quote.js
@@ -1,3 +1,6 @@
+import { customAlphabet } from 'nanoid'
+const nanoid = customAlphabet('1234567890abcdef', 10)
+
 const db = require("../lib/db.js");
 
 module.exports = {
@@ -12,8 +15,14 @@ module.exports = {
         })
     },
     async create(data) {
-        return await db.run(async function(mongodb) {
-            return await mongodb.collection('quotes').insertOne(data);
+        return await db.run(async function (mongodb) {
+            data.id = nanoid();
+            var result = await mongodb.collection('quotes').insertOne(data);
+            //todo: properly deal with failed insert
+            if (result.ok === 1) {
+                return true;
+            }
+            throw 'failed to insert';
         })
     },
 };
