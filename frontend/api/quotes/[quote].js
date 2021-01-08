64206a9f0464cfea15d37622036594fec2a1a510
Paul <p@ulmart.in>
2021-01-08 19:38:21 +0000

    rm vercel api routes

--- frontend/api/quotes/[quote].js
+++
@@ -1,6 +0,0 @@
-const quoteRepository = require("../../apisrc/repositories/quote.js");
-
-module.exports = async (req, res) => {
-  var results = await quoteRepository.findById(req.query.quote);
-  res.json(results)
-}
--- frontend/api/quotes/create.js
+++
@@ -1,6 +0,0 @@
-const quoteRepository = require("../../apisrc/repositories/quote.js");
-
-module.exports = async (req, res) => {
-  var results = await quoteRepository.create(req.query);
-  res.json(results)
-}
